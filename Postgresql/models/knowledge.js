781d7f46fff21bfe0a9d80dfb2d4da28a712959f
Osama Alghanmi <oalghnmi@gmail.com>
2024-05-23 01:59:09 -0400

    postgres fix

--- Postgresql/models/knowledge.js
+++ Postgresql/models/knowledge.js
@@ -12,7 +12,7 @@ let knowledgeSchema = new Schema({
   image: String,
   isASeed: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
-  resource: { type: String, default: "knowledge" }
+  resource: { type: String, default: "knowledges" }
 });
 knowledgeSchema.plugin(mongoosePaginate);
-module.exports = mongoose.model("Knowledge", knowledgeSchema);
+module.exports = mongoose.model("knowledges", knowledgeSchema);
